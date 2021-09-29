86c7f78886565749dc299903a6c56a1490a3028a
Andreas Koehler <andreaskoehler85@freenet.de>
2021-09-29 13:25:52 +0200

    erste Version

---
+++ src/HauptMenu.java
@@ -0,0 +1,20 @@
+
+public class HauptMenu {
+
+	public static void main(String[] args) {
+		
+		System.out.println("--------------------------------------------------\n");
+		System.out.println("MyGame\tHauptmenü\tvon:[Andreas Köhler]\n");
+		System.out.println("------------------------------\n");
+		System.out.println("1)Spiel starten\n");
+		System.out.println("2)Highscores\n");
+		System.out.println("3)Einstellungen\n");
+		System.out.println("4)Über MyGame\n");
+		System.out.println("0)Spiel beenden\n");
+		System.out.println("------------------------------\n");
+		System.out.println("Bitte wählen:\n");
+		
+
+	}
+
+}
